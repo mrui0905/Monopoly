251bcad942337f2ca8afcccab8899e102867f1a8
mrui0905 <133053196+mrui0905@users.noreply.github.com>
2023-06-02 09:47:42 -0600

    Commit 3

--- player.py
+++ player.py
@@ -6,6 +6,8 @@ class Player:
         self.money = 1500
         self.properties = set()
         self.jail_card = 0
+        self.imprisoned = False
+        self.rr = 0
 
     def pay_to(self, payee, amount):
         if amount > self.money:
@@ -14,5 +16,18 @@ class Player:
         self.money -= amount
         payee.money += amount
         return True
+    
+    def go_to_jail(self):
+        self.location = bd.Jail
+        self.imprisoned = True
+
+    def buy(self, property):
+        self.money -= property.cost
+        self.properties.add(property)
+        property.owner = self
+        if property.set == 'RR':
+            self.rr += 1
+
+
     
         
